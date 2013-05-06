7f36d649e197caaa885c10aa208a483007d0dec2
Willian Carvalho <o.chambs@gmail.com>
2013-05-05 21:54:01 -0300

    Adicionado dict com dados de usuarios e seus gostos por bandas Metodo get_all tem parametro prefs, default para critics Correção do metodo sim_distance, passand sqrt para a soma de potencias

--- recommendations.py
+++ recommendations.py
@@ -18,7 +18,13 @@ critics = {
 	'The Night Listener': 3.0, 'Superman Returns': 5.0, 'You, Me and Dupree': 3.5},
 	'Toby': {'Snakes on a Plane':4.5,'You, Me and Dupree':1.0,'Superman Returns':4.0}}
 
-people = [p for p in critics]
+#lista alternativa de pessoas e seus gostos com bandas
+teste = {
+	'Karen': {'Blind Guardian': 5.0, 'Metallica': 1.0, 'Black Sabbath': 4.9},
+	'Patricio': {'Blind Guardian': 1.2, 'Metallica': 4.7, 'Black Sabbath': 4.3},
+	'Filomeno': {'Blind Guardian': 4.5, 'Metallica': 1.1, 'Black Sabbath': 5},
+	'Zelao': {'Blind Guardian': 0, 'Metallica': 1.5, 'Black Sabbath': 4.9},
+}
 
 def sim_distance(prefs, person1, person2):
 
@@ -36,15 +42,17 @@ def sim_distance(prefs, person1, person2):
 	sum_of_squares = sum([pow(prefs[person1][item] - prefs[person2][item], 2) 
 		for item in prefs[person1] if item in prefs[person2]])
 
-	return 1 / (1 + sum_of_squares)
+	return 1 / (1 + sqrt(sum_of_squares))
 
 
 #parametro indica que o rating deve ser maior do que criteria
-def get_all(criteria = 0):
-	res = [(p1, p2, sim_distance(critics, p1, p2)) 
+def get_all(criteria = 0, prefs=critics):
+	people = [p for p in prefs]
+
+	res = [(p1, p2, sim_distance(prefs, p1, p2)) 
 	for p1 in people 
 	for p2 in people 
 	if p1 != p2
-	and sim_distance(critics, p1, p2) > criteria]
+	and sim_distance(prefs, p1, p2) > criteria]
 
 	return res
